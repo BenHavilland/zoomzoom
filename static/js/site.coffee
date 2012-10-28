0244d12ee62a3330dea88fb03732ffc2c589d09f
Ben Havilland <ben@havilland.net>
2012-10-27 22:27:50 -0700

    Added basic app framework: models, collections and some views.

--- static/js/site.coffee
+++ static/js/site.coffee
@@ -6,37 +6,97 @@ $ ->
 
   # Models
   # coming soon...
+  class Vehicle extends Backbone.Model
+
+    # make all vehicles awesome
+    defaults: ->
+      isAwesome: "yes"
+
+    # tell the browser console that we're taking the vehicle model for a spin
+    initialize: ->
+      console.log "vehicle initialized"
 
   # Collections
-  # coming soon...
+  class VehicleList extends Backbone.Collection
+
+    # reference vehicle model
+    model: Vehicle
+
+    # save the set under the "vehicles" namespace
+    localStorage: new Store("vehicles-backbone")
+
+  # create our global collection of Vehicles.
+  Vehicles = new VehicleList;
 
   # Views
   ToolBarView = Backbone.View.extend
     tagName: "div"
     className: "toolbar"
+
+    # will change to real template shortly
     template: "<p>This is just a local template doc  that will be \
     changed in the near future.<p/> \
     <p>Once we define out models we will add list items here like below.</p>
-    <h3>Cars</h3>
-    <ul>
-      <li>2001 Volvo V70 T5</li>
-      <li>1991 BMW 325ic</li>
-      <li>1976 Ford F350</li>
-    </ul>
-    <h3>Motorcycles</h3>
-    <ul>
-      <li>1980 Honda CB750</li>
-      <li>1980 Honda CM400T</li>
-      <li>1982 Honda CM250C</li>
-    </ul>
     "
 
     initialize: ->
       $('body').append @render().el
+      vehiclesView = new VehiclesView
+      $(@el).append vehiclesView.render().el
 
     render: ->
       $(@el).html @template
       @
+
+  VehicleView = Backbone.View.extend
+    tagName: "li"
+    className: "vehicle"
+
+    # will change to real template shortly
+    template: "Y{{year}} M{{make}} M{{model}}"
+
+    render: ->
+      $(@el).html @template
+      @
+
+  VehiclesView = Backbone.View.extend
+    # This will become a ul tag once we hook up the model control buttons
+    tagName: "div"
+    className: "vehicles"
+
+    # Here as placeholder content until we add some vehicles
+    template: "<h3>Cars</h3>
+      <ul>
+        <li>2001 Volvo V70 T5</li>
+        <li>1991 BMW 325ic</li>
+        <li>1976 Ford F350</li>
+      </ul>
+      <h3>Motorcycles</h3>
+      <ul>
+        <li>1980 Honda CB750</li>
+        <li>1980 Honda CM400T</li>
+        <li>1982 Honda CM250C</li>
+      </ul>"
+
+    initialize: ->
+      Vehicles.bind 'reset', @addAll, @
+      Vehicles.fetch()
+
+    render: ->
+      $(@el).html @template
+      @
+
+    addOne: (vehicle) ->
+      console.log 'here is one vehicle'
+      vehicleView = new VehicleView model:vehicle
+      $(@el).append(vehicleView.render().el);
+      @
+
+    addAll: ->
+      console.log 'polling all'
+      Vehicles.each(@addOne)
+      @
+
 
   # App Loader View
   # coming soon...
--- static/js/site.js
+++ static/js/site.js
@@ -1,17 +1,95 @@
 (function() {
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   $(function() {
-    var ToolBar, ToolBarView;
+    var ToolBar, ToolBarView, Vehicle, VehicleList, VehicleView, Vehicles, VehiclesView;
     $('body').fadeIn();
+    Vehicle = (function(_super) {
+
+      __extends(Vehicle, _super);
+
+      function Vehicle() {
+        Vehicle.__super__.constructor.apply(this, arguments);
+      }
+
+      Vehicle.prototype.defaults = function() {
+        return {
+          isAwesome: "yes"
+        };
+      };
+
+      Vehicle.prototype.initialize = function() {
+        return console.log("vehicle initialized");
+      };
+
+      return Vehicle;
+
+    })(Backbone.Model);
+    VehicleList = (function(_super) {
+
+      __extends(VehicleList, _super);
+
+      function VehicleList() {
+        VehicleList.__super__.constructor.apply(this, arguments);
+      }
+
+      VehicleList.prototype.model = Vehicle;
+
+      VehicleList.prototype.localStorage = new Store("vehicles-backbone");
+
+      return VehicleList;
+
+    })(Backbone.Collection);
+    Vehicles = new VehicleList;
     ToolBarView = Backbone.View.extend({
       tagName: "div",
       className: "toolbar",
-      template: "<p>This is just a local template doc  that will be \    changed in the near future.<p/> \    <p>Once we define out models we will add list items here like below.</p>    <h3>Cars</h3>    <ul>      <li>2001 Volvo V70 T5</li>      <li>1991 BMW 325ic</li>      <li>1976 Ford F350</li>    </ul>    <h3>Motorcycles</h3>    <ul>      <li>1980 Honda CB750</li>      <li>1980 Honda CM400T</li>      <li>1982 Honda CM250C</li>    </ul>    ",
+      template: "<p>This is just a local template doc  that will be \    changed in the near future.<p/> \    <p>Once we define out models we will add list items here like below.</p>    ",
       initialize: function() {
-        return $('body').append(this.render().el);
+        var vehiclesView;
+        $('body').append(this.render().el);
+        vehiclesView = new VehiclesView;
+        return $(this.el).append(vehiclesView.render().el);
       },
       render: function() {
         $(this.el).html(this.template);
+        return this;
+      }
+    });
+    VehicleView = Backbone.View.extend({
+      tagName: "li",
+      className: "vehicle",
+      template: "Y{{year}} M{{make}} M{{model}}",
+      render: function() {
+        $(this.el).html(this.template);
+        return this;
+      }
+    });
+    VehiclesView = Backbone.View.extend({
+      tagName: "div",
+      className: "vehicles",
+      template: "<h3>Cars</h3>      <ul>        <li>2001 Volvo V70 T5</li>        <li>1991 BMW 325ic</li>        <li>1976 Ford F350</li>      </ul>      <h3>Motorcycles</h3>      <ul>        <li>1980 Honda CB750</li>        <li>1980 Honda CM400T</li>        <li>1982 Honda CM250C</li>      </ul>",
+      initialize: function() {
+        Vehicles.bind('reset', this.addAll, this);
+        return Vehicles.fetch();
+      },
+      render: function() {
+        $(this.el).html(this.template);
+        return this;
+      },
+      addOne: function(vehicle) {
+        var vehicleView;
+        console.log('here is one vehicle');
+        vehicleView = new VehicleView({
+          model: vehicle
+        });
+        $(this.el).append(vehicleView.render().el);
+        return this;
+      },
+      addAll: function() {
+        console.log('polling all');
+        Vehicles.each(this.addOne);
         return this;
       }
     });
