f2bca3c8b1af4091a1c422a5f05d990603863905
Ben Havilland <ben@havilland.net>
2012-11-29 12:10:48 -0800

    Added some heroku relative path magic.

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('django.views.generic.simple',
     # Examples:
     # url(r'^$', 'website.views.home', name='home'),
     # url(r'^website/', include('website.foo.urls')),
@@ -14,4 +14,19 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'direct_to_template', {'template': 'index.html'}),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
+
+if not settings.DEBUG:
+   urlpatterns += patterns('',
+       url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+           'document_root': settings.STATIC_ROOT}),
+   )
